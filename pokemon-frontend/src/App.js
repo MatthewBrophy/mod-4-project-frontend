f2828c4f227d9e812defc034773e3735dbe3513d
matthewbrophy <m.broph@gmail.com>
2019-04-11 14:50:49 -0700

    landing page to home page

--- pokemon-frontend/src/App.js
+++ pokemon-frontend/src/App.js
@@ -1,46 +1,59 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import LandingPage from './LandingPage'
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import LandingPage from "./LandingPage";
+
+const AllPokemonURL = "http://localhost:3000/api/v1/pokemons";
 
 class App extends Component {
-  constructor(){
-    super()
+  constructor() {
+    super();
 
-    let exampleTrainer = {}
-    exampleTrainer.name = "kevin"
-    exampleTrainer.hometown = "pallet town"
-    exampleTrainer.age = 31
-    exampleTrainer.img = "https://img.rankedboost.com/wp-content/plugins/ice/pokemon/Luxio-Pokemon-Go.png"
-    exampleTrainer.enemy = "eli"
+    let exampleTrainer = {};
+    exampleTrainer.name = "kevin";
+    exampleTrainer.hometown = "pallet town";
+    exampleTrainer.age = 31;
+    exampleTrainer.img =
+      "https://img.rankedboost.com/wp-content/plugins/ice/pokemon/Luxio-Pokemon-Go.png";
+    exampleTrainer.enemy = "eli";
 
     this.state = {
-      allPokemon: [1,2,3, 4, 5, 6],
-      team: [1,2,3],
-      trainer: exampleTrainer,
-    }
+      allPokemon: [],
+      team: [1, 2, 3],
+      trainer: exampleTrainer
+    };
   }
+
+  componentDidMount() {
+    this.getAllPokemon();
+  }
+
+  getAllPokemon = () => {
+    fetch(AllPokemonURL)
+      .then(response => response.json())
+      .then(pokemon => this.setState({ allPokemon: pokemon }));
+  };
 
   getTeam = () => {
-    let url = ""
+    let url = "";
     fetch(url)
-    .then(data => data.json())
-    .then(result => {
-      this.setState({team: result})
-    })
-  }
+      .then(data => data.json())
+      .then(result => {
+        this.setState({ team: result });
+      });
+  };
 
-  addToTeam = (pokemon) => {
-    this.setState((prevState) => ({
+  addToTeam = pokemon => {
+    this.setState(prevState => ({
       team: [pokemon, ...prevState.team]
-    }))
-  }
+    }));
+  };
 
   render() {
     return (
-      <div >
+      <div>
+        {console.log(this.state.allPokemon)}
         <LandingPage />
-
       </div>
     );
   }
--- pokemon-frontend/src/LandingPage.js
+++ pokemon-frontend/src/LandingPage.js
@@ -1,18 +1,36 @@
 import React, { Component } from "react";
+
 import MainImage from "./images/maxresdefault.jpg";
+import Home from "./Home";
 
 class LandingPage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      redirect: false
+    };
+  }
+
+  handleOnClick = () => {
+    this.setState({ redirect: true });
+  };
+
   render() {
     return (
       <div>
         <img src={MainImage} alt="" />
         <br />
-        <button
-          className="enter-button"
-          onClick={() => this.props.RenderHomePage}
-        >
-          Lets Catch Some Pokemon
-        </button>
+        {this.state.redirect ? (
+          <Home />
+        ) : (
+          <button
+            className="enter-button"
+            onClick={this.handleOnClick}
+            type="button"
+          >
+            Lets Catch Some Pokemon
+          </button>
+        )}
       </div>
     );
   }
