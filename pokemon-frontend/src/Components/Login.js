2c52e6b475fb4f62d86b10ab14947f86344acd81
Jessica Betts <jessicaannbettsis@gmail.com>
2019-04-14 13:47:12 -0700

    building Login

--- pokemon-frontend/src/Components/Login.js
+++ pokemon-frontend/src/Components/Login.js
@@ -1,8 +1,71 @@
 import React, { Component } from "react";
+import { Redirect } from "react-router-dom";
+import Ash from "../images/ash.png";
+import Home from "../Containers/Home";
 
 class Login extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      name: "",
+      trainer: {},
+      redirect: false
+    }
+  }
+
+  getTrainer = () => {
+    let url = "http://localhost:3000/api/v1/trainers/";
+    fetch(url)
+      .then(res => res.json())
+      .then(
+        newTrainer => (
+          this.setState({ trainer: newTrainer, redirect: true }),
+          this.props.setTrainer(newTrainer)
+        )
+      );
+  };
+
+  submitForm = ev => {
+    ev.preventDefault();
+    this.createTrainer();
+  };
+
+  handleChange = ev => {
+    this.setState({
+      [ev.target.name]: ev.target.value
+    });
+  };
+
   render() {
-    return <div>Togglies</div>;
+    return this.state.redirect === true ? (
+      <Redirect to="/Home" />
+    ) : (
+      <div>
+        <div className="row justify-content-center align-items-center">
+          <label>Tell me about yourself!</label>
+        </div>
+        <div className="row justify-content-center">
+          <form
+            onSubmit={ev => this.submitForm(ev)}
+            className="form-horizontal text-right"
+          >
+            <label>Name:</label>
+            <input
+              onChange={ev => this.handleChange(ev)}
+              name="name"
+              type="text"
+            />
+            <br />
+            <input type="submit" />
+          </form>
+        </div>
+        <br />
+        <div className="row justify-content-center align-items-center">
+          <img src={Ash} />
+        </div>
+      </div>
+    );
   }
 }
 
