9d1bfebb48f86c4e687a3d45cfc84f43ec1b5839
matthewbrophy <m.broph@gmail.com>
2019-04-15 10:07:22 -0700

    merging changes

--- pokemon-frontend/src/Components/ChooseStarter.js
+++ pokemon-frontend/src/Components/ChooseStarter.js
@@ -12,32 +12,39 @@ class ChooseStarter extends Component {
       redirect: false,
       starters: [
         {
-        back_img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/4.png",
-        front_img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/4.png",
-        hp: 39,
-        id: 4,
-        name: "charmander",
-        weight: 85
-      },
-      {
-        back_img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/1.png",
-        front_img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png",
-        hp: 45,
-        id: 1,
-        name: "bulbasaur",
-        weight: 69
-      },
-      {
-        back_img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/7.png",
-        front_img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/7.png",
-        hp: 44,
-        id: 7,
-        name: "squirtle",
-        weight: 90
-      }]
+          back_img:
+            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/4.png",
+          front_img:
+            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/4.png",
+          hp: 39,
+          id: 4,
+          name: "charmander",
+          weight: 85
+        },
+        {
+          back_img:
+            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/1.png",
+          front_img:
+            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png",
+          hp: 45,
+          id: 1,
+          name: "bulbasaur",
+          weight: 69
+        },
+        {
+          back_img:
+            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/7.png",
+          front_img:
+            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/7.png",
+          hp: 44,
+          id: 7,
+          name: "squirtle",
+          weight: 90
+        }
+      ]
     };
 
-    console.log(this.state.starters)
+    console.log(this.state.starters);
   }
 
   submitForm = ev => {
@@ -50,7 +57,7 @@ class ChooseStarter extends Component {
     this.setState({
       selected_pokemon: this.state.starters[ev.target.value]
     });
-    console.log(this.state.selected_pokemon)
+    console.log(this.state.selected_pokemon);
   };
 
   render() {
@@ -61,32 +68,32 @@ class ChooseStarter extends Component {
         <form onSubmit={ev => this.submitForm(ev)}>
           <label>Name:</label>
           <div>
-           <img src={this.state.starters[0].front_img} />
-             <input
-              onChange={(ev) => this.handleChange(ev)}
-               name="starters"
-               type="radio"
-               value="0"
-             />
-         </div>
-         <div>
-          <img src={this.state.starters[1].front_img} />
+            <img src={this.state.starters[0].front_img} />
             <input
-              onChange={(ev) => this.handleChange(ev)}
+              onChange={ev => this.handleChange(ev)}
+              name="starters"
+              type="radio"
+              value="0"
+            />
+          </div>
+          <div>
+            <img src={this.state.starters[1].front_img} />
+            <input
+              onChange={ev => this.handleChange(ev)}
               name="starters"
               type="radio"
               value="1"
             />
-        </div>
-        <div>
-         <img src={this.state.starters[2].front_img} />
-           <input
-            onChange={(ev) => this.handleChange(ev)}
-             name="starters"
-             type="radio"
-             value="2"
-           />
-       </div>
+          </div>
+          <div>
+            <img src={this.state.starters[2].front_img} />
+            <input
+              onChange={ev => this.handleChange(ev)}
+              name="starters"
+              type="radio"
+              value="2"
+            />
+          </div>
 
           <input type="submit" />
         </form>
